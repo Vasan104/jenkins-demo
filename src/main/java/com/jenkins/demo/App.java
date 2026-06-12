f520b74273b2ca38680d2b0fafc18c618a547d65
Thirumalaivasan.S <166120802+Vasan104@users.noreply.github.com>
2026-06-12 16:42:47 +0530

    Add App class with main method and utility functions

---
+++ src/main/java/com/jenkins/demo/App.java
@@ -0,0 +1,16 @@
+package com.jenkins.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Maven Build!");
+        System.out.println("Build successful!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
